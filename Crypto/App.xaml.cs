ce31c14c892cf745ad6cd523017576cbd5a334e4
Nastya <anastasiia.solianyk@nure.ua>
2024-02-02 10:48:23 +0200

    Добавьте файлы проекта.

---
+++ Crypto/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Crypto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
