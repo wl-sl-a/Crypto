f174846f6da504e2ed939b7dfd95f6ee03ec865d
Nastya <anastasiia.solianyk@nure.ua>
2024-02-02 11:13:37 +0200

    Added folders Model, View, ViewModels

--- Crypto/View/MainWindow.xaml.cs
+++ Crypto/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Crypto
+namespace Crypto.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
