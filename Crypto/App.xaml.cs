b7143b1ab72e041fcaf165778b9e47343a469015
Nastya <anastasiia.solianyk@nure.ua>
2024-02-03 13:31:47 +0200

    added Dependency Injection

--- Crypto/App.xaml.cs
+++ Crypto/App.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
+using System.Text;
 using System.Windows;
+using Crypto.View;
+using Crypto.ViewModel;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Crypto
 {
@@ -9,6 +15,25 @@ namespace Crypto
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider? ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs eventArgs)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient(typeof(MainWindow));
+            serviceCollection.AddSingleton<NavigationViewModel>();
+            serviceCollection.AddSingleton<AssetsViewModel>();
+            serviceCollection.AddSingleton<SearchViewModel>();
+        }
     }
 
 }
